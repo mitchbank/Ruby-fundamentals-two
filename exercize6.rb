9c0853ccbcabe77bec147acdad21ec2c71a8dbb7
Mitchell Bank <mitchbank26@gmail.com>
2015-08-19 16:11:40 -0400

    you need to pick up bananas

--- exercize6.rb
+++ exercize6.rb
@@ -17,3 +17,9 @@ grocery_list << "rice"
 output(grocery_list)
 
 puts grocery_list.length
+
+if grocery_list.include?("bananas")
+	puts "You don't need to pick up bananas today"
+else
+	puts "You need to pick up bananas"
+end
