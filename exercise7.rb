c9a731289cb01a6d9d5d18dbf47f9054e0d28306
Mitchell Bank <mitchbank26@gmail.com>
2015-08-19 16:48:38 -0400

    step 1

---
+++ exercise7.rb
@@ -0,0 +1,14 @@
+def cohort_size(num_students)
+	num_students.each do |cohort, size|
+		puts "#{cohort} #{size} students"
+	end
+end
+
+
+students = {
+  cohort1: 34,
+  cohort2: 42,
+  cohort3: 22
+}
+
+cohort_size(students)
